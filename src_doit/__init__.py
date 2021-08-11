0424edfb49402c4231394b3e171182ea02bd3343
bozo.kopic <bozo@kopic.xyz>
2021-08-11 18:49:53 +0200

    doit update

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -5,6 +5,12 @@ import tempfile
 
 from hat import json
 from hat.doit import common
+from hat.doit.py import (build_wheel,
+                         run_pytest,
+                         run_flake8)
+from hat.doit.docs import (SphinxOutputType,
+                           build_sphinx,
+                           build_pdoc)
 
 
 __all__ = ['task_clean_all',
@@ -44,7 +50,7 @@ def task_build():
     """Build"""
 
     def build():
-        common.wheel_build(
+        build_wheel(
             src_dir=src_py_dir,
             dst_dir=build_py_dir,
             src_paths=list(common.path_rglob(src_py_dir,
@@ -53,7 +59,8 @@ def task_build():
             description='Hat Syslog',
             url='https://github.com/hat-open/hat-syslog',
             license=common.License.APACHE2,
-            packages=['hat'])
+            packages=['hat'],
+            console_scripts=['hat-syslog = hat.syslog.server.main:main'])
 
     return {'actions': [build],
             'task_dep': ['ui',
@@ -62,41 +69,24 @@ def task_build():
 
 def task_check():
     """Check with flake8"""
-    return {'actions': [(_run_flake8, [src_py_dir]),
-                        (_run_flake8, [pytest_dir])]}
+    return {'actions': [(run_flake8, [src_py_dir]),
+                        (run_flake8, [pytest_dir])]}
 
 
 def task_test():
     """Test"""
-
-    def run(args):
-        common.mkdir_p(ui_dir)
-        subprocess.run([sys.executable, '-m', 'pytest',
-                        '-s', '-p', 'no:cacheprovider',
-                        *(args or [])],
-                       cwd=str(pytest_dir),
-                       check=True)
-
-    return {'actions': [run],
+    return {'actions': [lambda args: run_pytest(pytest_dir, *(args or []))],
             'pos_arg': 'args',
             'task_dep': ['json_schema_repo']}
 
 
 def task_docs():
     """Docs"""
-
-    def build():
-        common.sphinx_build(common.SphinxOutputType.HTML, docs_dir,
-                            build_docs_dir)
-        subprocess.run([sys.executable, '-m', 'pdoc',
-                        '--html', '--skip-errors', '-f',
-                        '-o', str(build_docs_dir / 'py_api'),
-                        'hat.syslog'],
-                       stdout=subprocess.DEVNULL,
-                       stderr=subprocess.DEVNULL,
-                       check=True)
-
-    return {'actions': [build],
+    return {'actions': [(build_sphinx, [SphinxOutputType.HTML,
+                                        docs_dir,
+                                        build_docs_dir]),
+                        (build_pdoc, ['hat.syslog',
+                                      build_docs_dir / 'py_api'])],
             'task_dep': ['ui',
                          'json_schema_repo']}
 
@@ -141,11 +131,6 @@ def task_json_schema_repo():
     return {'actions': [generate],
             'file_dep': src_paths,
             'targets': [json_schema_repo_path]}
-
-
-def _run_flake8(path):
-    subprocess.run([sys.executable, '-m', 'flake8', str(path)],
-                   check=True)
 
 
 _webpack_conf = r"""
